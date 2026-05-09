31def6d0c6f35842285b85d4d4281308501839e6
Lewin Kelly <heli@odex.cf>
2026-05-09 23:26:44 +0100

    Add page and controller for completed orders, link from cart page

---
+++ src/Controller/OrdersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Database;
+use Symfony\Component\HttpFoundation\{Request, Response};
+use Symfony\Component\Routing\Attribute\Route;
+
+final class OrdersController extends Base
+{
+	#[Route("/orders", methods: ["GET"], name: "orders", options: ["sitemap" => true])]
+	final public function home(Request $request): Response
+	{
+		$user = $this->user($request);
+		if (!$user)
+			return $this->redirectToLogin($request);
+
+		$cart = Database::getCart($user->id, true);
+
+		$total = 0;
+		foreach ($cart as $item)
+			$total += $item->price * $item->quantity;
+
+		return $this->finish($request, "orders.html.twig", [
+			"cart" => $cart,
+			"total" => $total,
+		]);
+	}
+}
